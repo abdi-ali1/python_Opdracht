05204493e7c4f22613cb0af5fa2623d9112aa23e
Abdi Ali <62744253+abdi-ali1@users.noreply.github.com>
2022-09-04 22:36:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ measuredTempaturesJune = [17.2, 18.9, 19.1, 19, 25.3, 26.8, 24.9, 25.9, 16.1, 19
 measuredTempaturesJuly = [28, 28.5, 29, 29.1, 23.1, 24.3, 19.1, 18.6, 20.3, 19.9, 19.9, 24, 26, 26, 29.8, 22.2, 21.4, 22, 23.1, 21, 27.2, 28.9, 29.2, 26.6, 27.9, 27.1, 32.1, 30.1, 29.9, 25 ]
 
 
-easyway = True 
+easyway = False 
 
+# gets the highest value from list
 def highesTemperature(temperatures, easyCode):
     highestnumber = 0    
     if easyCode:
@@ -19,6 +20,8 @@ def highesTemperature(temperatures, easyCode):
         
     return highestnumber
 
+
+#gets the lowest value from paramater list
 def lowestTemperature(temperatures, easyway):
     lowestNumber = 50
     if easyway:
@@ -29,9 +32,13 @@ def lowestTemperature(temperatures, easyway):
                 lowestNumber = temp
     return lowestNumber
 
-def calculateRange(temperatures):
-    return abs((highesTemperature(temperatures,easyway)) - (lowestTemperature(temperatures,easyway)))
 
+# calculates the average ranges from parameter list
+def calculateRange(temperatures):
+    return highesTemperature(temperatures,easyway) - lowestTemperature(temperatures,easyway)
+
+    
+# calculate average value from paramater list
 def calculateAverageTempature(temperatures):
       averagenumber = 0
       for temp in temperatures:
@@ -50,26 +57,22 @@ def calculateHeateDays(temperatures):
           
     
 def calculateHighestJump(temperatures):
-    HighestJump = 0
-    day1= 0
-    day2= 0
-    listdata = []
-    for i in range(1, len(temperatures) - 1):
-  
-        diff = temperatures[i] - temperatures[i + 1]
-      
-        if HighestJump <= diff:
-                HighestJump = diff
-                day1 = temperatures.index(temperatures[i - 3])
-                day2 = temperatures.index(temperatures[i + 1 - 3])
-                
-                
-                
-    listdata.append(round(HighestJump, 1))
-    listdata.append(day1)
-    listdata.append(day2)
-    
-    return listdata
+    highestJump = 0
+    day1 = 0
+    day2 = 0
+    listData = []
+
+    for i in range(0, len(temperatures) - 1):
+        difference = temperatures[i] - temperatures[i+1]
+        if highestJump < difference:
+            highestJump = difference
+            day1 = i+1
+            day2 = i+2
+
+    listData.append(round(highestJump, 1))
+    listData.append(day1)
+    listData.append(day2)
+    return listData
 
 
 
@@ -101,7 +104,7 @@ print(f'There was a heat wave in June ({calculateHeateDays(measuredTempaturesJul
 
 #ORIENTING: Assessment assignment part 5
 print("ORIENTING: Assessment assignment part 4 ")
-print(f'The biggest jump in June was {calculateHighestJump(measuredTempaturesJune)[0]} degrees. It occurred from  day {calculateHighestJump(measuredTempaturesJune)[1]}  to day {calculateHighestJump(measuredTempaturesJuly)[2]}')
+print(f'The biggest jump in June was {calculateHighestJump(measuredTempaturesJune)[0]} degrees. It occurred from  day {calculateHighestJump(measuredTempaturesJune)[1]}  to day {calculateHighestJump(measuredTempaturesJune)[2]}')
 print(f'The biggest jump in July was {calculateHighestJump(measuredTempaturesJuly)[0]} degrees. It occurred from day {calculateHighestJump(measuredTempaturesJuly)[1]} to day {calculateHighestJump(measuredTempaturesJuly)[2]} ')
 
 
